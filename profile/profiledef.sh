2dd3ea0daab540174b08b0c97572f14459360392
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2024-11-17 14:55:36 +0500

    Add Source Code

---
+++ profile/profiledef.sh
@@ -0,0 +1,16 @@
+iso_name="mcali"
+iso_label="MCALI[devtest 0.002]$(date +%Y%m)"
+iso_publisher="M-Unity Software <https://site.test>"
+iso_application="M-Unity's Custom Arch Linux ISO"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/root"]="0:0:750"
+)
