52d6f00867e39def773ab1b09b0a4f3eea41f5e5
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2025-02-19 23:07:02 +0500

    start.sh update and new configs
    fish and wlogout

--- profile/airootfs/ast/scr/start.sh
+++ profile/airootfs/ast/scr/start.sh
@@ -1,2 +1,9 @@
 #/bin/bash env
+echo "Installing YAY..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sid --noconfirm
+cd $HOME
+echo "Installing AUR Packages..."
+yay -S wlogout emote superfile
 exec hyprland
